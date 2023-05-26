370a8940543ae1471dfb22229f54d07cd4b6bc89
Anthony Susevski <77211520+asusevski@users.noreply.github.com>
2023-05-26 13:55:08 -0400

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,16 @@
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of sks Yorkie" \
+  --class_prompt="a photo of Yorkie" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=300 \
+  --max_train_steps=1000
